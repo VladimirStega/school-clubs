bf1afc995509e53a365ed544f49f24f082c8fa44
VladimirStega03 <114366736+VladimirStega03@users.noreply.github.com>
2024-09-29 09:57:21 +0300

    Updates

--- assets/BtnLinkHome-e8e60a67.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as n,f as _,w as s,e as c}from"./index-cfd445df.js";const r={};function a(i,l){const e=t("RouterLink");return n(),_(e,{class:"home-link",to:"/"},{default:s(()=>[c("Прейти на главну страницу")]),_:1})}const f=o(r,[["render",a]]);export{f as B};
